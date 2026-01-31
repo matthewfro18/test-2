af2f60c3e4966c44b0b08f1091f385b235713ccc
matthewfro181818 <158477647+matthewfro181818@users.noreply.github.com>
2026-01-31 04:43:35 -0500

    Update field.lua

--- example_mods/vs eevee/stages/field.lua
+++ example_mods/vs eevee/stages/field.lua
@@ -3,4 +3,9 @@ function onCreate()
     setScrollFactor('field', 1, 1);
     scaleObject('field', 1, 1);
     addLuaSprite('field', false);
+    
+    makeLuaSprite('mic', 'game/mic', 465, 582);
+    setScrollFactor('mic', 1, 1);
+    scaleObject('mic', 1, 1);
+    addLuaSprite('mic', false);
 end
