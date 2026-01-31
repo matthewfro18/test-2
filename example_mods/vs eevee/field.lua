62c81eb4609d0cb867b7708407b687f8a3252935
matthewfro181818 <158477647+matthewfro181818@users.noreply.github.com>
2026-01-31 04:38:44 -0500

    Create field.lua

---
+++ example_mods/vs eevee/field.lua
@@ -0,0 +1,6 @@
+function onCreate()
+    makeLuaSprite('field', 'stages/field', -904, -493);
+    setScrollFactor('field', 1, 1);
+    scaleObject('field', 1, 1);
+    addLuaSprite('field', false);
+end
